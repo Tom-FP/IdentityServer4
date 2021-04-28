2b2669f1781389b96849a725a6b9c222aa306a54
Tom Andersen <Tom.Andersen@formpipe.com>
2021-04-28 14:00:19 +0200

    WCF Client building

--- samples/Clients/src/WcfClient/Program.cs
+++ samples/Clients/src/WcfClient/Program.cs
@@ -1,7 +1,6 @@
-﻿using IdentityModel.Client;
+using IdentityModel.Client;
 using IdentityModel.Constants;
 using IdentityModel.Extensions;
-using Sample;
 using System;
 using System.Security.Cryptography;
 using System.Text;
@@ -13,6 +12,19 @@ using WcfService;
 
 namespace WcfClient
 {
+    public static class Constants
+    {
+        public const string BaseAddress = "https://localhost:5001";
+
+        public const string AuthorizeEndpoint = BaseAddress + "/connect/authorize";
+        public const string LogoutEndpoint = BaseAddress + "/connect/endsession";
+        public const string TokenEndpoint = BaseAddress + "/connect/token";
+        public const string UserInfoEndpoint = BaseAddress + "/connect/userinfo";
+        public const string IdentityTokenValidationEndpoint = BaseAddress + "/connect/identitytokenvalidation";
+        public const string TokenRevocationEndpoint = BaseAddress + "/connect/revocation";
+
+    }
+
     class Program
     {
         static void Main(string[] args)
